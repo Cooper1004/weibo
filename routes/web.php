df48178da43607823e6c99570c6e04fbc3dc4240
Cooper1004 <jdarcy710@gmail.com>
2023-04-11 17:34:45 +0800

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return config('db.username');
-    return view('welcome');
-});
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
