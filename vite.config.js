5efcf7a8eb54366d78f2be4ff7979244a2f9aaac
Cooper1004 <jdarcy710@gmail.com>
2023-04-12 17:16:44 +0800

    初始化样式

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+   plugins: [
+       laravel([
+           'resources/js/app.js',
+       ]),
+   ],
+   server: {
+    host: "0.0.0.0",
+    hmr: {
+        host: "localhost"
+    }
+},
+   resolve: {
+       alias: {
+           '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+       }
+   },
 });
